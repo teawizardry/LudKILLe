34ebfe8cfdedc11618ab23769cc3a0f161960e95
Hannah MK <youhoo1234@gmail.com>
2023-09-11 10:50:17 -0400

    made sw dice rolls emojis

--- ludkille.py
+++ ludkille.py
@@ -218,6 +218,7 @@ async def sw(ctx, dice=None):
     Example usage: !sw [ability, proficiency, boost, difficulty, challenge, setback]
     """
     allowed_dice = {"ability": ability, "proficiency": proficiency, "boost": boost, "difficulty": difficulty, "challenge": challenge, "setback": setback, "force": force}
+    dice_emojis = {"Advantage": "<:advantage:1150616126850596864>", "Blank": "<:blank:1150616129925034027>", "Dark": "<:dark:1150616134173859851>", "Despair": "<:despair:1150616453838544978>", "Failure": "<:failure:1150616458196426883>", "Light": "<:light:1150616461056950314>", "Success": "<:success:1150616144613494787>", "Threat": "<:threat:1150616464148144128>", "Triumph": "<:triumph:1150616465653891173>"}
     
     if dice in allowed_dice:
         result = sw_roll(allowed_dice[dice])
@@ -225,25 +226,20 @@ async def sw(ctx, dice=None):
         await ctx.send("Error please pick valid dice. `!help_me sw` for more info.")
         return
        
-    result_img = ''
-    description = '**Result:** '
+    description = '**Result: **'
+    embed = discord.Embed(title=f"{ctx.message.author.display_name} rolled a {dice} die", description=description, color = discord.Colour.random())
+    
     for r in result: 
-        result_img += f"{r}_"
-        description += f"{r} "
-    result_img += '.png'
-    result_img_path = f"./imgs/{result_img}"
-    roll_file = discord.File(result_img_path, filename=result_img)
+        embed.add_field(name=f"{r}", value=f"{dice_emojis[r]}", inline=True)
     
     dice_img = f'{dice}.png'
     dice_img_path = f"./imgs/{dice_img}"
     dice_file = discord.File(dice_img_path, filename=dice_img)
     
-    embed = discord.Embed(title=f"{ctx.message.author.display_name} rolled a {dice} die", description=description, color = discord.Colour.random())
     embed.set_author(name=ctx.message.author.display_name, icon_url=ctx.message.author.avatar)
     embed.set_thumbnail(url=f"attachment://{dice_img}")
-    embed.set_image(url=f"attachment://{result_img}")
     
-    await ctx.send(files=[roll_file, dice_file], embed=embed)
+    await ctx.send(file=dice_file, embed=embed)
 
   
 #################################      
